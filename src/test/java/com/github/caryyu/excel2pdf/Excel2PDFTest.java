3e2857b5c8a65a062f95bfcf6dbfc24c9157b3ee
cary.yu <343194291@qq.com>
2018-01-05 18:11:08 +0800

    待改进计划说明

--- src/test/java/com/github/caryyu/excel2pdf/Excel2PDFTest.java
+++ src/test/java/com/github/caryyu/excel2pdf/Excel2PDFTest.java
@@ -3,10 +3,7 @@ package com.github.caryyu.excel2pdf;
 import com.itextpdf.text.DocumentException;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +20,21 @@ public class Excel2PDFTest {
         objects.add(new ExcelObject("1.MAD 5-3-05-Octavia NF-20131025.xls",fis1));
         objects.add(new ExcelObject("2.MAD 6-1-47-Octavia NF-20131025.xls",fis2));
         objects.add(new ExcelObject("3.MAD 038-Superb FL DS-20131025.xls",fis3));
+
+        Excel2Pdf pdf = new Excel2Pdf(objects , fos);
+
+        pdf.convert();
+    }
+    
+    @Test
+    public void testSingle() throws IOException, DocumentException {
+        FileInputStream in = new FileInputStream("/Users/cary/Desktop/naked innovation talent managment.xlsx");
+
+        List<ExcelObject> objects = new ArrayList<ExcelObject>();
+        objects.add(new ExcelObject("1.MAD 5-3-05-Octavia NF-20131025.xls",in));
+
+        FileOutputStream fos = new FileOutputStream(new File("/Users/cary/Desktop/naked innovation talent managment.pdf"));
+
 
         Excel2Pdf pdf = new Excel2Pdf(objects , fos);
 
