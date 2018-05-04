d678b84f869c712b07a47eef8604d00f6ccaaf9c
RayHu <huyi0912@hotmail.com>
2018-05-04 11:01:02 +0800

    新增 getPageSize()

--- src/main/java/com/github/caryyu/excel2pdf/ExcelObject.java
+++ src/main/java/com/github/caryyu/excel2pdf/ExcelObject.java
@@ -2,6 +2,10 @@ package com.github.caryyu.excel2pdf;
 
 import java.io.InputStream;
 
+import org.apache.poi.ss.usermodel.*;
+
+import com.itextpdf.text.*;
+
 /**
  * Created by cary on 6/15/17.
  */
@@ -43,5 +47,35 @@ public class ExcelObject {
     }
     Excel getExcel() {
         return excel;
+    }
+    
+    /**
+     * 取得 Excel 版面設定的紙張大小並依據直橫式旋轉
+     * @return
+     */
+    public Rectangle getPageSize() {
+    	short size = excel.getSheet().getPrintSetup().getPaperSize();
+    	boolean landScape = excel.getSheet().getPrintSetup().getLandscape();
+    	switch(size) {
+    	case PrintSetup.A3_PAPERSIZE:
+    		return landScape?PageSize.A3.rotate():PageSize.A3;
+    	case PrintSetup.A4_PAPERSIZE:
+    		return landScape?PageSize.A4.rotate():PageSize.A4;
+    	case PrintSetup.A4_ROTATED_PAPERSIZE:
+        	return landScape?PageSize.A4:PageSize.A4.rotate();
+        case PrintSetup.A4_SMALL_PAPERSIZE:
+           	return landScape?PageSize.A4.rotate():PageSize.A4;
+        case PrintSetup.A5_PAPERSIZE:
+           	return landScape?PageSize.A5.rotate():PageSize.A5;
+        case PrintSetup.LETTER_PAPERSIZE:
+    		return landScape?PageSize.LETTER.rotate():PageSize.LETTER;
+        case PrintSetup.LETTER_ROTATED_PAPERSIZE:
+    		return landScape?PageSize.LETTER:PageSize.LETTER.rotate();
+        case PrintSetup.B4_PAPERSIZE:
+           	return landScape?PageSize.B4.rotate():PageSize.B4;
+        case PrintSetup.B5_PAPERSIZE:
+           	return landScape?PageSize.B5.rotate():PageSize.B5;
+    	default: return landScape?PageSize.A4.rotate().rotate():PageSize.A4;
+    	}
     }
 }
