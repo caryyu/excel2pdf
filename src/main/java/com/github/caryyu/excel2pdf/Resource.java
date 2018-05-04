3ab05cc8fb53f96766209e2941c25d7ccc06dce4
RayHu <huyi0912@hotmail.com>
2018-05-04 11:03:05 +0800

    新增 getFont
    1.將 POI Font 轉換到 iText Font
    2.預先載入系統內字型(慢)

--- src/main/java/com/github/caryyu/excel2pdf/Resource.java
+++ src/main/java/com/github/caryyu/excel2pdf/Resource.java
@@ -1,5 +1,8 @@
 package com.github.caryyu.excel2pdf;
 
+import org.apache.poi.hssf.usermodel.*;
+
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
 
 /**
@@ -9,12 +12,29 @@ public class Resource {
     /**
      * 中文字体支持
      */
-    protected static BaseFont BASE_FONT_CHINESE;
+   protected static BaseFont BASE_FONT_CHINESE;
     static {
         try {
             BASE_FONT_CHINESE = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+            // 搜尋系統,載入系統內的字型(慢)
+            FontFactory.registerDirectories();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    /**
+     * 將 POI Font 轉換到 iText Font
+     * @param font
+     * @return
+     */
+    public static com.itextpdf.text.Font getFont(HSSFFont font) {
+    	try {
+    		com.itextpdf.text.Font iTextFont =FontFactory.getFont(font.getFontName(), BaseFont.IDENTITY_H, BaseFont.EMBEDDED,font.getFontHeightInPoints());
+    		return iTextFont;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	return null;
     }
 }
